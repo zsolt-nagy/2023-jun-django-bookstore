caa34942168ed330d03b9ce1495f8086428aeaf7
Zsolt Nagy <info@zsoltnagy.eu>
2023-10-08 17:33:13 +0200

    book details feature

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('store/', views.store, name='store'),
+    path('store/<int:book_id>', views.book_details, name='book_details'),
 ]
--- store/views.py
+++ store/views.py
@@ -13,10 +13,27 @@ def index(request):
 
 
 def store(request):
-    count = Book.objects.all().count()
+    books = Book.objects.all()
+    count = books.count()
     context = {
         'title': 'Mystery Books Store',
+        'books': books,
         'count': count,
         'menu': 'store',
     }
     return render(request, 'store/store.html', context)
+
+
+def book_details(request, book_id):
+    context = {
+        'menu': 'store',
+        'has_error': False,
+    }
+    try:
+        book = Book.objects.get(id=book_id)
+        context['title'] = book.title
+        context['book'] = book
+    except:
+        context['title'] = 'The book you are looking for is not available'
+        context['has_error'] = True
+    return render(request, 'store/book_details.html', context)
